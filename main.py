2a8abc66cbf56f5e3cc0796b0922434fb2be7747
sk <sebastian.kuppa@web.de>
2023-05-30 13:41:56 +0700

    added window, in-loop filling

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 import pygame
-from time import sleep
+
+WIDTH = 800
+HEIGHT = 600
+BACKGROUND = (0, 0, 0)
 
 
 def main():
-    pass
+    pygame.init()
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    clock = pygame.time.Clock()
+
+    while True:
+        screen.fill(BACKGROUND)
+        pygame.display.flip()
+
+        clock.tick(60)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
