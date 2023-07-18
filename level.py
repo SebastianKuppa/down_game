56a9de7ca5e670b4167bfce73fb4d34944559bc0
sk <sebastian.kuppa@web.de>
2023-07-18 19:09:26 +0200

    added player to game

--- level.py
+++ level.py
@@ -1,5 +1,6 @@
 import pygame
 from tiles import Tile
+from player import Player
 from settings import tile_size
 
 
@@ -8,9 +9,11 @@ class Level:
         self.display_surface = surface
         self.level_data = level_data
         self.setup_level(level_data)
+        self.world_shift = 1
 
     def setup_level(self, layout):
         self.tiles = pygame.sprite.Group()
+        self.player = pygame.sprite.Group()
         for row_index, row in enumerate(layout):
             for col_index, cell in enumerate(row):
                 if cell == 'X':
@@ -18,7 +21,14 @@ class Level:
                     y = row_index * tile_size
                     tile = Tile((x, y), tile_size)
                     self.tiles.add(tile)
+                if cell == 'P':
+                    x = col_index * tile_size
+                    y = row_index * tile_size
+                    p1 = Player((x, y))
+                    self.player.add(p1)
 
     def run(self):
-        self.tiles.update(1)
+        self.tiles.update(self.world_shift)
         self.tiles.draw(self.display_surface)
+        self.player.update(self.world_shift)
+        self.player.draw(self.display_surface)
--- tiles.py
+++ tiles.py
@@ -5,7 +5,7 @@ class Tile(pygame.sprite.Sprite):
     def __init__(self, pos, size):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.Surface([size, size])
-        self.image.fill('red')
+        self.image.fill('grey')
         self.rect = self.image.get_rect(topleft=pos)
 
     def update(self, x_shift):
