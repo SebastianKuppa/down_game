ba51b523ec703f1890e67c0dce7140fba61f7faa
sk <sebastian.kuppa@web.de>
2023-07-18 19:20:10 +0200

    added player movement

--- level.py
+++ level.py
@@ -9,26 +9,25 @@ class Level:
         self.display_surface = surface
         self.level_data = level_data
         self.setup_level(level_data)
-        self.world_shift = 1
+        self.world_shift = 0
 
     def setup_level(self, layout):
         self.tiles = pygame.sprite.Group()
         self.player = pygame.sprite.Group()
         for row_index, row in enumerate(layout):
             for col_index, cell in enumerate(row):
+                x = col_index * tile_size
+                y = row_index * tile_size
+
                 if cell == 'X':
-                    x = col_index * tile_size
-                    y = row_index * tile_size
                     tile = Tile((x, y), tile_size)
                     self.tiles.add(tile)
                 if cell == 'P':
-                    x = col_index * tile_size
-                    y = row_index * tile_size
                     p1 = Player((x, y))
                     self.player.add(p1)
 
     def run(self):
         self.tiles.update(self.world_shift)
         self.tiles.draw(self.display_surface)
-        self.player.update(self.world_shift)
+        self.player.update()
         self.player.draw(self.display_surface)
