cbb33876305c5c522f77f21bf7c078f1218e59f2
sk <sebastian.kuppa@web.de>
2023-08-14 22:59:25 +0200

    fixed bug for pressing space and arrow at the same time

--- player.py
+++ player.py
@@ -24,8 +24,12 @@ class Player(pygame.sprite.Sprite):
     def get_input(self):
         keys = pygame.key.get_pressed()
         if keys[pygame.K_RIGHT]:
+            if keys[pygame.K_SPACE]:
+                self.jump()
             self.direction.x = 1
         elif keys[pygame.K_LEFT]:
+            if keys[pygame.K_SPACE]:
+                self.jump()
             self.direction.x = -1
         elif keys[pygame.K_SPACE]:
             self.jump()
